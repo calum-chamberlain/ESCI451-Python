9704da182dc7069cd1a824c26b6bc8dc04bdc8ab
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2023-02-14 17:09:30 +1300

    Make asc_to_df work with ESRI files

--- helpers/asc_to_df.py
+++ helpers/asc_to_df.py
@@ -1,4 +1,4 @@
-# Taken from https://gist.github.com/joshhartmann11
+# Adapted from https://gist.github.com/joshhartmann11
 
 import numpy as np
 import pandas as pd
@@ -9,8 +9,8 @@ def transform_asc_to_df(ascfile):
 
     # Split into header and body
     with open(ascfile, "r") as f:
-        header = [next(f) for x in range(N_ROWS_HEADER)]
-    body = np.loadtxt(ascfile, skiprows=N_ROWS_HEADER)
+        contents = f.read().splitlines()
+        header, body_raw = contents[0:N_ROWS_HEADER], contents[N_ROWS_HEADER:]
 
     # Read the configuration from the header
     config = {}
@@ -18,26 +18,39 @@ def transform_asc_to_df(ascfile):
         variable = header[i].strip().split(" ")[0]
         value = float(header[i].strip().split(" ")[-1])
         config[variable.lower()] = value
+        
+    # Hack to cope (slowly) with somewhat irregular grid
+    body = np.zeros([int(config["nrows"]), int(config["ncols"])])
+    
+    row, col = 0, 0
+    for line in body_raw:
+        if line == '':
+            row += 1
+            col = 0
+        elems = line.split()
+        for elem in elems:
+            body[row][col] = float(elem)
+            col += 1
 
     # Put NAN where applicable
     body[body == config["nodata_value"]] = float('nan')
 
     # Generate lat lon columns
-    start = config["cellsize"]/2
+    start = config["cellsize"] / 2
     step = config["cellsize"]
 
-    latstart = config["yllcorner"] + step/2
-    latstop = latstart + step * (config["nrows"]-1)
+    latstart = config["yllcorner"] + step / 2
+    latstop = latstart + step * (config["nrows"] - 1)
     latitude = np.linspace(latstart, latstop, int(config["nrows"]))
     latitude = np.repeat(latitude, int(config["ncols"]))[::-1]
 
-    lonstart = config["xllcorner"] + step/2
-    lonstop = lonstart + step * (config["ncols"]-1)
+    lonstart = config["xllcorner"] + step / 2
+    lonstop = lonstart + step * (config["ncols"] - 1)
     longitude = np.linspace(lonstart, lonstop, int(config["ncols"]))
     longitude = np.tile(longitude, int(config["nrows"]))
 
     # Generate grid points
-    grid_y = np.linspace(0, config["nrows"]-1, int(config["nrows"]))
+    grid_y = np.linspace(0, config["nrows"] - 1, int(config["nrows"]))
     grid_y = np.repeat(grid_y, int(config["ncols"]))[::-1]
 
     grid_x = np.linspace(0, config["ncols"]-1, int(config["ncols"]))
